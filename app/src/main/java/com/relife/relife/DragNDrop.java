4fb5cef1eda640b0cf09c9e01f67a0e4f2753239
Onur GÜNEŞ <onur.gunes@tedu.edu.tr>
2016-11-19 23:08:20 +0200

    bisiler yaptim

--- app/src/main/java/com/relife/relife/DragNDrop.java
+++ app/src/main/java/com/relife/relife/DragNDrop.java
@@ -3,13 +3,16 @@ package com.relife.relife;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.DragEvent;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,29 +31,82 @@ public class DragNDrop extends Activity {
     static LinearLayout[] layouts = new LinearLayout[24];
     static TextView[] textViews = new TextView[24];
     static int lastLayout = -1;
-
     static Context context;
+    static Drawable[] drawables = new Drawable[6];
+    static String[] drawableDesc = new String[6];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag_ndrop);
-        ImageView image = (ImageView) findViewById(R.id.imageView3);
-        image.setImageResource(R.mipmap.ic_launcher);
+        context = getApplicationContext();
+        //ImageView image = (ImageView) findViewById(R.id.imageView3);
+        //image.setImageResource(R.mipmap.ic_launcher);
         for(int i = 0; i < layouts.length; i++){
             int resID = getResources().getIdentifier("i"+i, "id", getPackageName());
             layouts[i] = (LinearLayout) findViewById(resID);
         }
         for(int i = 0; i < layouts.length; i++){
             TextView tv = new TextView(this);
-            tv.setText("" + i);
-            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            tv.setText(i + "");
+            tv.setTextColor(Color.parseColor("#9D5B4D"));
+            tv.setTextSize(18);
+            tv.setGravity(Gravity.CENTER);
+            layouts[i].setGravity(Gravity.CENTER);
             layouts[i].addView(tv);
             textViews[i] = tv;
         }
         for(int i = 0; i < layouts.length; i++){
             layouts[i].setOnDragListener(new dragListener());
         }
-        findViewById(R.id.imageView3).setOnTouchListener(new touchListener());
+        LinearLayout scrollLayout = new LinearLayout(this);
+        scrollLayout.setOrientation(LinearLayout.VERTICAL);
+        drawables[0] = getDrawable(R.drawable.sleep);
+        drawables[1] = getDrawable(R.drawable.gamecontroller);
+        drawables[2] = getDrawable(R.drawable.openbook);
+        drawables[3] = getDrawable(R.drawable.knight);
+        drawables[4] = getDrawable(R.drawable.soccer);
+        drawables[5] = getDrawable(R.drawable.dumbbell);
+        drawableDesc[0] = "Sleep";
+        drawableDesc[1] = "Game";
+        drawableDesc[2] = "Study";
+        drawableDesc[3] = "Chess";
+        drawableDesc[4] = "Soccer";
+        drawableDesc[5] = "Work Out";
+        for(int i = 0; i < drawables.length; i++){
+            LinearLayout ll = new LinearLayout(this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
+            param.setMargins(50, 50, 30, 0);
+            ll.setLayoutParams(param);
+            ll.setGravity(Gravity.CENTER_VERTICAL);
+            ImageView img = new ImageView(this);
+            img.setImageDrawable(drawables[i]);
+            img.setScaleX(0.5f);
+            img.setScaleY(0.5f);
+            img.setOnTouchListener(new touchListener());
+            TextView text = new TextView(this);
+            text.setText("Activity Name: " + drawableDesc[i]);
+            text.setTextSize(16);
+            ll.addView(img);
+            ll.addView(text);
+            scrollLayout.addView(ll);
+        }
+        ScrollView scrollView = (ScrollView) findViewById(R.id.activities);
+        scrollView.addView(scrollLayout);
+        Button button = (Button) findViewById(R.id.done);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bad_intention = new Intent();
+                bad_intention.putExtra("result", "your message");
+                setResult(RESULT_OK, bad_intention);
+                finish();
+            }
+        });
+        //findViewById(R.id.imageView3).setOnTouchListener(new touchListener());
     }
 }
 
@@ -85,25 +141,24 @@ class dragListener implements View.OnDragListener {
             case DragEvent.ACTION_DRAG_EXITED:
                 break;
             case DragEvent.ACTION_DROP:
-                View view = (View) event.getLocalState();
-                ViewGroup owner = (ViewGroup) view.getParent();
-                owner.removeView(view);
-
+                ImageView view = (ImageView) event.getLocalState();
                 LinearLayout container = (LinearLayout) v;
                 container.removeAllViews();
-                container.addView(view);
-
-                if(DragNDrop.lastLayout != -1){
-                    owner.addView(DragNDrop.textViews[DragNDrop.lastLayout]);
-                }
-
-                for(int i = 0; i < DragNDrop.layouts.length; i++){
-                    if(container.getId() == DragNDrop.layouts[i].getId()){
-                        DragNDrop.lastLayout = i;
-                        break;
-                    }
-                }
-
+                ImageView imageView = new ImageView(DragNDrop.context);
+                imageView.setImageDrawable(view.getDrawable());
+                imageView.setScaleX(0.5f);
+                imageView.setScaleY(0.5f);
+                container.addView(imageView);
+//                if(DragNDrop.lastLayout != -1){
+//                    owner.addView(DragNDrop.textViews[DragNDrop.lastLayout]);
+//                }
+//
+//                for(int i = 0; i < DragNDrop.layouts.length; i++){
+//                    if(container.getId() == DragNDrop.layouts[i].getId()){
+//                        DragNDrop.lastLayout = i;
+//                        break;
+//                    }
+//                }
                 view.setVisibility(View.VISIBLE);
                 break;
             case DragEvent.ACTION_DRAG_ENDED:
--- app/src/main/java/com/relife/relife/MainActivity.java
+++ app/src/main/java/com/relife/relife/MainActivity.java
@@ -3,7 +3,9 @@ package com.relife.relife;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -26,8 +28,55 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DragNDrop.class);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                LinearLayout sakuya = (LinearLayout) findViewById(R.id.list);
+                sakuya.removeAllViews();
+                String result = data.getStringExtra("result");
+                for(int i = 0; i < DragNDrop.layouts.length; i++){
+                    View view = DragNDrop.layouts[i].getChildAt(0);
+                    if (view instanceof ImageView) {
+                        ImageView imageView = (ImageView) view;
+                        int which = -1;
+                        for(int j = 0; j < DragNDrop.drawables.length; j++){
+                            if(DragNDrop.drawables[j].equals(imageView.getDrawable())){
+                                which = j;
+                                break;
+                            }
+                        }
+                        if(which!=-1){
+                            LinearLayout patchouli = (LinearLayout) findViewById(R.id.list);
+                            LinearLayout knowledge = new LinearLayout(this);
+                            knowledge.setOrientation(LinearLayout.HORIZONTAL);
+                            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
+                            param.setMargins(50, 50, 30, 0);
+                            knowledge.setLayoutParams(param);
+                            knowledge.setGravity(Gravity.CENTER_VERTICAL);
+                            ImageView img = new ImageView(this);
+                            img.setImageDrawable(DragNDrop.drawables[which]);
+                            img.setScaleX(0.5f);
+                            img.setScaleY(0.5f);
+                            TextView text = new TextView(this);
+                            text.setText("Time: " + i + ":00 Activity Name: " + DragNDrop.drawableDesc[which]);
+                            text.setTextSize(16);
+                            knowledge.addView(img);
+                            knowledge.addView(text);
+                            patchouli.addView(knowledge);
+                        }
+                    }
+                }
+            }
+            if (resultCode == RESULT_CANCELED) {
+                // Write your code if there's no result
+            }
+        }
     }
 }
